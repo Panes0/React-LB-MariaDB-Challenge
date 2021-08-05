141647a322a8bfa5827b1653743cce8dac04814e
manu naga <nagami.mn@gmail.com>
2021-08-05 20:27:07 -0300

    from uploaded csv file now parses the file and stores it into DB

--- api/src/controllers/file-upload.controller.ts
+++ api/src/controllers/file-upload.controller.ts
@@ -7,7 +7,12 @@ import {
     RestBindings,
 } from "@loopback/rest";
 import { FILE_UPLOAD_SERVICE } from "../keys";
-import { FileUploadHandler } from "../types";
+import { FileUploadHandler, MovieObj } from "../types";
+
+import fs from "fs";
+import path from "path";
+import csv from "csv-parser";
+import axios from "axios";
 
 /**
  * A controller to handle file uploads using multipart/form-data media type
@@ -65,7 +70,7 @@ export class FileUploadController {
         let files: object[] = [];
 
         ///////TESTING////////////////////
-        console.log(request.files);
+        //console.log(files);
         ///////////////////////////
 
         if (Array.isArray(uploadedFiles)) {
@@ -75,6 +80,59 @@ export class FileUploadController {
                 files.push(...uploadedFiles[filename].map(mapper));
             }
         }
+
+        FileUploadController.parseAndLoadToDB();
+
         return { files, fields: request.body };
+    }
+
+    static async parseAndLoadToDB() {
+        //parse CSV file
+        const results: any = [];
+        let normalized_results: MovieObj[] = [];
+        let new_file_path = path.join(
+            __dirname,
+            "../../",
+            `${process.env.UPLOAD_DIR}/peliculas.csv`,
+        );
+        fs.createReadStream(new_file_path)
+            .pipe(
+                csv({
+                    headers: ["title", "description", "release"],
+                    separator: ";",
+                }),
+            )
+            .on("data", data => results.push(data))
+            .on("end", async () => {
+                console.log(results);
+                normalized_results = results.map((movie: any) => {
+                    const norm_movie = {
+                        title: movie.title,
+                        description: movie.description,
+                        release: movie.release,
+                    };
+                    console.log("NORM MOVIE", norm_movie);
+                    return norm_movie;
+                });
+                
+                //Send data to DB
+                for (let movie of normalized_results) {
+                    axios
+                        .post(
+                            "http://localhost:3000/movies",
+                            JSON.stringify(movie),
+                            {
+                                headers: {
+                                    "Content-Type": "application/json",
+                                },
+                            },
+                        )
+                        .catch(function (error) {
+                            console.log("ERROR: ", error.response.data);
+                        });
+
+                    //console.log(movie);
+                }
+            });
     }
 }
--- api/src/types.ts
+++ api/src/types.ts
@@ -1,3 +1,9 @@
 import {RequestHandler} from 'express-serve-static-core';
 
 export type FileUploadHandler = RequestHandler;
+
+export interface MovieObj {
+    title: string;
+    description?: string;
+    release?: string;
+}
