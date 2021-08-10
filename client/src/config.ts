1ef40c59eac919dcd8ebc7538c7662c648f0ee4f
manu naga <nagami.mn@gmail.com>
2021-08-09 21:23:49 -0300

    added readme

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,2 +1,3 @@
-export const BACK_URL = "http://localhost:3001";
+export const BACK_URL = "http://18.228.7.127/api";
+//export const BACK_URL = "http://localhost:3001";
 export const PAGE_LIMIT = 4;
